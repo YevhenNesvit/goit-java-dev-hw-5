d0d273b5df34511780eb410d392f34f0244baf0c
Yevhen <yevhen.nesvit@gmail.com>
2022-10-02 18:22:35 +0300

    Add PutUserByUsername command

--- src/main/java/services/StoreService.java
+++ src/main/java/services/StoreService.java
@@ -76,8 +76,22 @@ public class StoreService {
         connection.setRequestMethod("DELETE");
         connection.setRequestProperty("Content-Type", "application/json");
 
-//        int responseCode = connection.getResponseCode();
+        int responseCode = connection.getResponseCode();
 //        System.out.println("DELETE response code: " + responseCode);
+        StringBuffer response = new StringBuffer();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in =
+                    new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response);
+        } else {
+            System.out.println("DELETE request not worked");
+        }
     }
 
     public Map<String, Integer> getPetStatusesList() throws IOException {
